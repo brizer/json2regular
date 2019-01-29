41af51bd3d3e5c32352f26bfc9552572f8946cb6
刘放 <362512489@qq.com>
2019-01-29 16:42:04 +0800

    nejdefine拼接流程走通

---
+++ dist/component.js
@@ -0,0 +1,25 @@
+NEJ.define([
+    pool/component-base/src/base,
+    pool/component-base/src/util,
+    pool/component-dropdown/src/dropdown/ui,
+    pool/component-input/src/input/ui,
+    pool/component-input/src/textarea/ui,
+    pool/component-check/src/radio-group/web/ui,
+    pool/component-check/src/check/ui,
+    pool/component-check/src/check-group/ui,
+    pool/component-calendar/src/dateTimePicker/ui,
+    
+],function(
+    Component,
+    util,
+    DropdownUI,
+    InputUI,
+    TexraretUI,
+    RadioGroupUI,
+    CheckUI,
+    CheckGroupUI,
+    dateTimePickerUI,
+    
+){
+    
+});
